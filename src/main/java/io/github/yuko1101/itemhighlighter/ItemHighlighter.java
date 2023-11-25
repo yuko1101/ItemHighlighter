64b5f2a36bcc386e392e98b47d07400fea4a9238
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-11-25 12:25:26 +0900

    Initial commit

---
+++ src/main/java/io/github/yuko1101/itemhighlighter/ItemHighlighter.java
@@ -0,0 +1,11 @@
+package io.github.yuko1101.itemhighlighter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemHighlighter implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
