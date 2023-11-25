c47fb0ce9184a9161ade800d5ba324766630292d
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-11-25 19:29:42 +0900

    add config file for needlessItems

--- src/main/kotlin/io/github/yuko1101/itemhighlighter/ItemHighlighter.kt
+++ src/main/kotlin/io/github/yuko1101/itemhighlighter/ItemHighlighter.kt
@@ -1,18 +1,33 @@
 package io.github.yuko1101.itemhighlighter
 
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import io.github.yuko1101.itemhighlighter.util.ConfigFile
 import net.fabricmc.api.ClientModInitializer
+import net.minecraft.item.Item
 import net.minecraft.item.Items
+import java.io.File
 
 @Suppress("unused")
 object ItemHighlighter : ClientModInitializer {
 
-    // TODO: add more items
-    val needlessItems = arrayListOf(
-        Items.WOODEN_SHOVEL, Items.WOODEN_PICKAXE, Items.WOODEN_AXE, Items.WOODEN_HOE, Items.WOODEN_SWORD,
-        Items.STONE_SHOVEL, Items.STONE_PICKAXE, Items.STONE_AXE, Items.STONE_HOE, Items.STONE_SWORD,
-    )
+    private val configFile: ConfigFile = ConfigFile(File("config/item-highlighter.json"), JsonObject().apply { add("needlessItems", JsonArray()) } )
+    val needlessItems: ArrayList<Item> = arrayListOf()
 
     override fun onInitializeClient() {
-
+        loadConfig()
     }
+
+    // TODO: add reload command
+    private fun loadConfig() {
+        needlessItems.clear()
+        needlessItems.addAll(configFile.load().getValue("needlessItems").asJsonArray.map {
+            try {
+                Items::class.java.getField(it.asString).get(null) as Item
+            } catch (e: NoSuchFieldException) {
+                Items.AIR
+            }
+        }.filter { it != Items.AIR })
+    }
+
 }
---
+++ src/main/kotlin/io/github/yuko1101/itemhighlighter/util/ConfigFile.kt
@@ -0,0 +1,164 @@
+package io.github.yuko1101.itemhighlighter.util
+
+import com.google.gson.*
+import java.io.File
+
+open class ConfigFile(val file: File, val default: JsonObject, private val route: ArrayList<String> = arrayListOf()) {
+    open var data: JsonObject = default
+    fun save(compact: Boolean = true): ConfigFile {
+        if (file.parentFile?.exists() == false) file.parentFile?.mkdirs()
+        if (compact) file.writeText(data.toString())
+        else file.writeText(GsonBuilder().serializeNulls().setPrettyPrinting().create()
+            .toJson(data))
+        return this
+    }
+    fun load(): ConfigFile {
+        if (!file.exists()) {
+            save()
+        }
+        try {
+            data = JsonParser().parse(file.readText()).asJsonObject
+        } catch (e: JsonSyntaxException) {
+            e.printStackTrace()
+            save() //ファイルを変更前に戻す
+        }
+        return this
+    }
+
+    fun set(key: String, value: JsonElement): ConfigFile {
+        getObjectFromRoute().add(key, value)
+        return this
+    }
+    fun set(key: String, value: String): ConfigFile {
+        getObjectFromRoute().addProperty(key, value)
+        return this
+    }
+    fun set(key: String, value: Number): ConfigFile {
+        getObjectFromRoute().addProperty(key, value)
+        return this
+    }
+    fun set(key: String, value: Boolean): ConfigFile {
+        getObjectFromRoute().addProperty(key, value)
+        return this
+    }
+    fun set(value: JsonElement): ConfigFile {
+        getPreObjectFromRoute().add(route.last(), value)
+        return this
+    }
+    fun set(value: String): ConfigFile {
+        getPreObjectFromRoute().addProperty(route.last(), value)
+        return this
+    }
+    fun set(value: Number): ConfigFile {
+        getPreObjectFromRoute().addProperty(route.last(), value)
+        return this
+    }
+    fun set(value: Boolean): ConfigFile {
+        getPreObjectFromRoute().addProperty(route.last(), value)
+        return this
+    }
+
+    fun getValue(key: String): JsonElement {
+        return getObjectFromRoute().get(key)
+    }
+    fun getValue(): JsonElement {
+        return getPreObjectFromRoute().get(route.last())
+    }
+
+    open fun get(key: String): PathResolver {
+        val newRoute = arrayListOf<String>()
+        newRoute.addAll(route)
+        newRoute.add(key)
+        return PathResolver(this, newRoute)
+    }
+    open fun getPath(path: List<String>): PathResolver {
+        val newRoute = arrayListOf<String>()
+        newRoute.addAll(route)
+        newRoute.addAll(path)
+        return PathResolver(this, newRoute)
+    }
+
+    fun has(key: String): Boolean {
+        return getObjectFromRoute().has(key)
+    }
+
+    fun hasPath(path: List<String>): Boolean {
+        var obj = getObjectFromRoute()
+        for (i in path.indices) {
+            if (!obj.has(path[i])) return false
+            obj = obj.get(path[i]).asJsonObject
+        }
+        return true
+    }
+
+    fun exists(): Boolean {
+        return hasPathInData(route)
+    }
+
+    fun resetData(): ConfigFile {
+        data = default
+        return this
+    }
+
+    fun resetPath(): ConfigFile {
+        route.clear()
+        return this
+    }
+
+    private fun getObjectFromRoute(): JsonObject {
+//        println(route)
+//        println(data)
+        var obj = data
+        for (i in 0 until route.size) {
+            obj = obj.get(route[i]).asJsonObject
+        }
+        return obj
+    }
+
+    private fun getPreObjectFromRoute(): JsonObject {
+        //println(route)
+        //println(data)
+        var obj = data
+        for (i in 0 until route.size - 1) {
+            obj = obj.get(route[i]).asJsonObject
+        }
+        return obj
+    }
+
+    private fun hasPathInData(path: List<String>): Boolean {
+        var obj = data
+        for (i in path.indices) {
+            if (!obj.has(path[i])) return false
+            obj = obj.get(path[i]).asJsonObject
+        }
+        return true
+    }
+
+    class PathResolver(private val configFile: ConfigFile, val route: ArrayList<String>) : ConfigFile(configFile.file, configFile.default, route) {
+        override var data: JsonObject
+            get() = configFile.data
+            set(value) {
+                configFile.data = value
+            }
+
+        override fun get(key: String): PathResolver {
+            val newRoute = arrayListOf<String>()
+            newRoute.addAll(route)
+            newRoute.add(key)
+            return PathResolver(configFile, newRoute)
+        }
+
+        override fun getPath(path: List<String>): PathResolver {
+            val newRoute = arrayListOf<String>()
+            newRoute.addAll(route)
+            newRoute.addAll(path)
+            return PathResolver(configFile, newRoute)
+        }
+    }
+
+    companion object {
+        fun File.asConfigFile(default: JsonObject): ConfigFile {
+            return ConfigFile(this, default)
+        }
+    }
+}
